609d76039cc48f4e1f08452d188f02948253fc0f
crisandolindesmanrumahorbo <crisandolin007@gmail.com>
2025-09-29 19:45:26 +0700

    add styling button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,17 @@
 import React, { ButtonHTMLAttributes } from "react";
 
-export type IButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+export interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+}
 
 export const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
-  ({ children = "", ...props }: IButtonProps, forwardedRef) => {
+  ({ children = "", className = "", ...props }: IButtonProps, forwardedRef) => {
     return (
       <button
         ref={forwardedRef}
-        className="px-4 py-1.5 text-base font-head transition-all rounded outline-hidden cursor-pointer duration-200 font-medium 
+        className={`px-4 py-1.5 text-base font-head transition-all rounded outline-hidden cursor-pointer duration-200 font-medium 
         flex items-center shadow-md hover:shadow-none bg-primary text-primary-foreground border-2 border-black 
-        hover:translate-y-1 hover:bg-primary-hover"
+        hover:translate-y-1 hover:bg-primary-hover ${className}`}
         {...props}
       >
         {children}
